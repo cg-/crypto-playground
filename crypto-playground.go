ec503a841f23cbd6c36f53962aadc9a25c4280f4
Cole Grim <cg@netfla.me>
2017-04-15 15:33:41 -0700

    some work

--- crypto-playground.go
+++ crypto-playground.go
@@ -1,40 +1,40 @@
 /*
 Crypto Playground is just me playing around with crypto/hashing concepts
 from class to make sure I understand them...
- */
+*/
 
 package main
 
 import (
-   "github.com/cg-/crypto-playground/rsa"
-   "github.com/op/go-logging"
-   "os"
+	"github.com/cg-/crypto-playground/rsa"
+	"github.com/op/go-logging"
+	"os"
 )
 
 var log = logging.MustGetLogger("crypto-playground")
 var format = logging.MustStringFormatter(
-   `%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
-
-func init(){
-   backend := logging.NewLogBackend(os.Stderr, "", 0)
-   backendFormatter := logging.NewBackendFormatter(backend, format)
-   //backendLeveled := logging.AddModuleLevel(backend)
-   //backendLeveled.SetLevel(logging.INFO, "")
-   logging.SetBackend(backendFormatter)
-   log.Info("Finished main init function.")
+func init() {
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+	backendFormatter := logging.NewBackendFormatter(backend, format)
+	//backendLeveled := logging.AddModuleLevel(backend)
+	//backendLeveled.SetLevel(logging.INFO, "")
+	logging.SetBackend(backendFormatter)
+	log.Info("Finished main init function.")
 }
-
 
 func main() {
-   log.Info("Starting main program.")
-   rsa.RSAGen()
+	log.Info("Starting main program.")
+	kp := rsa.RSAGen(2048)
+	log.Info(kp.String())
+	log.Info(kp.PrivKey.Encrypt([]byte{1, 2, 8}))
 }
 
-func check(e error){
-   if e != nil {
-      log.Fatalf(e.Error())
-      os.Exit(5)
-   }
+func check(e error) {
+	if e != nil {
+		log.Fatalf(e.Error())
+		os.Exit(5)
+	}
 }
--- rsa/rsa.go
+++ rsa/rsa.go
@@ -1,61 +1,122 @@
 package rsa
 
 import (
-   "math/rand"
-   "strconv"
-   "github.com/op/go-logging"
-   "time"
-   "math/big"
+	"fmt"
+	"github.com/op/go-logging"
+	"math/big"
+	"math/rand"
+	"time"
 )
+
+type KeyPair struct {
+	Size    int
+	PubKey  Key
+	PrivKey Key
+}
+
+func (k *Key) Encrypt(b []byte) []byte {
+	log.Infof("Encrypting %v", b)
+	toRet := []byte{}
+	res := big.NewInt(0)
+	for i := range b {
+		m := int8(b[i])
+		log.Infof("m: %v", m)
+		log.Infof("m 64: %v", int64(m))
+		res.Exp(big.NewInt(int64(m)), k.Exponent, nil)
+		res.Mod(res, k.Mod)
+		log.Info(res)
+	}
+	return toRet
+}
+
+func (kp *KeyPair) String() string {
+	s1 := ""
+	s1 += fmt.Sprintf("PubKey START\n%s\nPubKey END", kp.PubKey.String())
+	s1 += fmt.Sprintln()
+	s1 += fmt.Sprintf("PrivKey START\n%s\nPrivKey END", kp.PrivKey.String())
+	return s1
+}
+
+type Key struct {
+	Size     int
+	Exponent *big.Int
+	Mod      *big.Int
+}
+
+func (k *Key) String() string {
+	s1 := ""
+	s1 += fmt.Sprintf("Exponent: [%v]\n", k.Exponent)
+	s1 += fmt.Sprintf("Mod: [%v]\n", k.Mod)
+	return s1
+}
 
 var log = logging.MustGetLogger("crypto-playground")
 
-func RSAGen() {
-   p := getPrime(time.Now().UnixNano())
-   q := getPrime(time.Now().UnixNano())
+func RSAGen(size int) *KeyPair {
+	p := getPrime(time.Now().UnixNano(), size)
+	q := getPrime(time.Now().UnixNano(), size)
 
-   log.Infof("P: %v Q: %v", p, q)
+	n := big.NewInt(0)
+	n.Mul(p, q)
+
+	pMinus1 := big.NewInt(0)
+	pMinus1.Sub(p, big.NewInt(1))
+
+	qMinus1 := big.NewInt(0)
+	qMinus1.Sub(p, big.NewInt(1))
+
+	toilent := big.NewInt(0)
+	toilent.Mul(pMinus1, qMinus1)
+
+	d := big.NewInt(0)
+	d.ModInverse(big.NewInt(65537), n)
+
+	toRet := &KeyPair{
+		Size: size,
+		PubKey: Key{
+			Size:     size,
+			Mod:      n,
+			Exponent: big.NewInt(65537),
+		},
+		PrivKey: Key{
+			Size:     size,
+			Mod:      d,
+			Exponent: big.NewInt(65537),
+		},
+	}
+	return toRet
 }
 
 /*
 getPrime generates a large prime number by making a bunch of 64 bit numbers and appending
 them, ensures it's odd, then tests it for primality probabilistically. If it's not prime,
 we add 2 to get another odd number, and repeat.
- */
-func getPrime(seed int64) *big.Int {
-   rand.Seed(seed)
-   v1 := strconv.FormatInt(rand.Int63(), 10)
-   v2 := strconv.FormatInt(rand.Int63(), 10)
-   v3 := strconv.FormatInt(rand.Int63(), 10)
-   v4 := strconv.FormatInt(rand.Int63(), 10)
-   v5 := strconv.FormatInt(rand.Int63(), 10)
-   v6 := strconv.FormatInt(rand.Int63(), 10)
-   v7 := strconv.FormatInt(rand.Int63(), 10)
-   v8 := strconv.FormatInt(rand.Int63(), 10)
-   v := v1 + v2 + v3 + v4 +v5 +v6 +v7 + v8
-   log.Infof("Generated random 64 bit numbers. Appended to: %v", v)
-   bigV := big.NewInt(0)
-   bigV.SetString(v, 10)
-   log.Infof("Generated big int: %v bit length: %v", bigV, bigV.BitLen())
+*/
+func getPrime(seed int64, size int) *big.Int {
+	rand.Seed(seed)
+	randArray := make([]byte, size/8)
+	rand.Read(randArray)
+	bigV := big.NewInt(0)
+	bigV.SetBytes(randArray)
+	log.Infof("Generated big int: %v bit length: %v", bigV, bigV.BitLen())
 
-   evenTest := big.NewInt(2)
+	evenTest := big.NewInt(2)
 
-   log.Info("Testing if it's even...")
-   evenTest.Mod(bigV, evenTest)
-   if evenTest.Int64() == 0 {
-      log.Info("It's even. Adding 1.")
-      bigV = bigV.Add(bigV, big.NewInt(1))
-   }
+	log.Info("Testing if it's even...")
+	evenTest.Mod(bigV, evenTest)
+	if evenTest.Int64() == 0 {
+		log.Info("It's even. Adding 1.")
+		bigV = bigV.Add(bigV, big.NewInt(1))
+	}
 
-   log.Infof("Proceeding with: %v", bigV)
+	log.Infof("Proceeding with: %v", bigV)
 
-   for !bigV.ProbablyPrime(10) {
-      log.Info("Testing for prime: %v", bigV)
-      log.Info("Probably not prime! Adding 2.")
-      bigV = bigV.Add(bigV, big.NewInt(2))
-   }
+	for !bigV.ProbablyPrime(10) {
+		log.Info("Testing for prime: %v", bigV)
+		log.Info("Probably not prime! Adding 2.")
+		bigV = bigV.Add(bigV, big.NewInt(2))
+	}
 
-   log.Infof("%v is probably prime.", bigV)
-   return bigV
+	log.Infof("%v is probably prime.", bigV)
+	return bigV
 }
-
